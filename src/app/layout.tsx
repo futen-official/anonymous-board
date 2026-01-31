a2956fcfbef9e918f0f781d3c5a78ace32baf188
仲村天地 <nakamuratenchi@MacBook-Pro.local>
2026-01-31 16:53:56 +0900

    chore: add google search console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,19 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <head>
+        {/* Google Search Console 所有権確認 */}
+        <meta
+          name="google-site-verification"
+          content="U3rgvTMTOMOMHrD2ocE4ADOS6OrePh_VYuIycJ3OUT8"
+        />
+
+        {/* Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700;800&family=Nunito:wght@400;600;700;800&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
